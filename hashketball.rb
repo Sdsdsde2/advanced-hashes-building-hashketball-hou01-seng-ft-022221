b1b69f47d33b60af2dd621e08358288005479a3c
Shane R <imspammails@gmail.com>
2021-02-01 21:04:20 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -54,14 +54,14 @@ def game_hash
     :slam_dunks => 1}]},
     :away => {:team_name => "Charlotte Hornets",
     :colors => ["Turquoise", "Purple"], :players => [{
-    :player_name => "Brook Lopez",
-    :number => 11,
-    :shoe => 17,
-    :points => 17,
-    :rebounds => 19,
-    :assists => 10,
-    :steals => 3,
-    :blocks => 1,
-    :slam_dunks => 15}
+    :player_name => "Jeff Adrian",
+    :number => 4,
+    :shoe => 18,
+    :points => 10,
+    :rebounds => 1,
+    :assists => 1,
+    :steals => 2,
+    :blocks => 7,
+    :slam_dunks => 2}
     }] }}
 end
