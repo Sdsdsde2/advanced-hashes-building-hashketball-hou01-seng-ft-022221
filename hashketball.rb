e45affa8804597dd5b87e458053f7505316c62f5
Shane R <imspammails@gmail.com>
2021-02-01 21:06:37 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -82,6 +82,16 @@ def game_hash
     :assists => 12,
     :steals => 4,
     :blocks => 5,
-    :slam_dunks => 5}]}
+    :slam_dunks => 5},
+    {
+    :player_name => "Brook Lopez",
+    :number => 11,
+    :shoe => 17,
+    :points => 17,
+    :rebounds => 19,
+    :assists => 10,
+    :steals => 3,
+    :blocks => 1,
+    :slam_dunks => 15}]}
   }
 end
